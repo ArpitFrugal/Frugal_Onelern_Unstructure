c9cd6dbc1b1ff28e916792ac9fe9258c309b3b57
ShashankFrugal <shashank@frugaltesting.com>
2022-10-10 10:25:59 +0530

    Added Lesson Delivery

--- src/test/java/Assessment/CreateAssessment_Online.java
+++ src/test/java/Assessment/CreateAssessment_Online.java
@@ -454,9 +454,11 @@ public class CreateAssessment_Online extends Base {
     // functions.
     @DataProvider(name = "teacherdata")
     public Object[][] getteacherData() throws FileAlreadyExistsException {
-//        Object loginData[][] =  {{"9000000101", "123456"}, {"9000000105", "123456"}, {"9000000109", "123456"},
-//                {"9000000113", "123456"}, {"9000000117", "123456"}};
-        Object loginData[][] = {{"9000000101", "123456"}};
+        Object loginData[][] =  {
+//                {"9000000101", "123456"},
+                {"9000000105", "123456"}, {"9000000109", "123456"},
+                {"9000000113", "123456"}, {"9000000117", "123456"}};
+//        Object loginData[][] = {{"9000000101", "123456"}};
         return loginData;
     }
 }
